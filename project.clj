e24ff475174e81c8acd9c22aff0015fe6592e58b
Paul DeLong <Paul.DeLong@gmail.com>
2016-06-14 08:49:25 -0400

    adding/updating dependencies;

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject whatzit-puzzle "0.1.0-SNAPSHOT"
    :license { :name "The MIT License (MIT)" }
-   :dependencies [[org.clojure/clojure "1.7.0"]]
+   :dependencies [
+      [org.clojure/clojure            "1.8.0"]
+      [org.clojure/math.combinatorics "0.1.3"]  ]
    :main ^:skip-aot whatzit-puzzle.core
    :target-path "target/%s"
    :profiles {:uberjar {:aot :all}}  )
+
