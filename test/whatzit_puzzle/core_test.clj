a543d13d0f71d2d5fe15be5cb7c0b1b156783a2b
Paul DeLong <Paul.DeLong@gmail.com>
2016-06-27 18:17:19 -0400

    starting to test smaller cases;
    Testing the code on smaller cases is a more sane approach, before
    tackling the full-sized puzzle.

--- test/whatzit_puzzle/core_test.clj
+++ test/whatzit_puzzle/core_test.clj
@@ -3,6 +3,10 @@
       [clojure.test        :refer :all]
       [whatzit-puzzle.core :refer :all]  )  )
 
+(def board-1x1 [[1]])
+
+(def pieces-1x1 [[[1]]])
+
 (deftest board-rows (testing "Board rows" (is (= 8 (count board)))))
 
 (deftest pieces-count (testing "Pieces count" (is (= 13 (count pieces)))))
@@ -11,5 +15,5 @@
 
 (deftest orientations
    (testing "Orientations"
+      (is (= [1] (rotation-counts pieces-1x1)))
       (is (= [1 4 2 4 4 4 2 4 4 4 4 2 2] (rotation-counts pieces)))  )  )
-
