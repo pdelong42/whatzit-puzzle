e902b502d6c389804942be4bcb5f13106480db47
Paul DeLong <Paul.DeLong@gmail.com>
2016-06-29 18:26:49 -0400

    testing with more ways to slice up the 2x2 board;

--- test/whatzit_puzzle/core_test.clj
+++ test/whatzit_puzzle/core_test.clj
@@ -20,6 +20,17 @@
    [  [  [1 1]  ]
       [  [1 1]  ]  ]  )
 
+(def pieces-2x2c
+   [  [  [1 1]  ]
+      [   [1]   ]
+      [   [1]   ]  ]  )
+
+(def pieces-2x2d
+   [  [  [1]  ]
+      [  [1]  ]
+      [  [1]  ]
+      [  [1]  ]  ]  )
+
 (def pieces-1x1-wrapped [[[[1]]]])
 
 (def pieces-2x2a-wrapped
@@ -30,6 +41,17 @@
 (def pieces-2x2b-wrapped
   [  [  [  [1 1]  ]  ]
      [  [  [1 1]  ]  ]  ]  )
+
+(def pieces-2x2c-wrapped
+   [  [  [  [1 1]  ]  ]
+      [  [   [1]   ]  ]
+      [  [   [1]   ]  ]  ]  )
+
+(def pieces-2x2d-wrapped
+   [  [  [  [1]  ]  ]
+      [  [  [1]  ]  ]
+      [  [  [1]  ]  ]
+      [  [  [1]  ]  ]  ]  )
 
 (def rotation-counts #(vec (map count (generate-rotations-all-pieces %))))
 
@@ -44,17 +66,24 @@
       (is (= 1  (count pieces-1x1)))
       (is (= 2  (count pieces-2x2a)))
       (is (= 2  (count pieces-2x2b)))
+      (is (= 3  (count pieces-2x2c)))
+      (is (= 4  (count pieces-2x2d)))
       (is (= 13 (count pieces)))  )  )
 
 (deftest vector-wrapper
    (testing "Vector Wrapper"
       (is (= pieces-1x1-wrapped  (wrap-each-piece-in-vector pieces-1x1)))
       (is (= pieces-2x2a-wrapped (wrap-each-piece-in-vector pieces-2x2a)))
-      (is (= pieces-2x2b-wrapped (wrap-each-piece-in-vector pieces-2x2b)))  )  )
+      (is (= pieces-2x2b-wrapped (wrap-each-piece-in-vector pieces-2x2b)))
+      (is (= pieces-2x2c-wrapped (wrap-each-piece-in-vector pieces-2x2c)))
+      (is (= pieces-2x2d-wrapped (wrap-each-piece-in-vector pieces-2x2d)))
+      (comment "placeholder for master list of pieces")  )  )
 
 (deftest orientations
    (testing "Orientations"
       (is (= (rotation-counts pieces-1x1)  [1]))
       (is (= (rotation-counts pieces-2x2a) [4 1]))
       (is (= (rotation-counts pieces-2x2b) [2 2]))
+      (is (= (rotation-counts pieces-2x2c) [2 1 1]))
+      (is (= (rotation-counts pieces-2x2d) [1 1 1 1]))
       (is (= (rotation-counts pieces)      [1 4 2 4 4 4 2 4 4 4 4 2 2]))  )  )
